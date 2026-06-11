59acbb8b94ea7b046f848deea6628821ba68a8ff
utama1955 <redminotee5190@gmail.com>
2026-06-11 10:27:31 +0700

    Add R2 upload URL to config.js

--- config.js
+++ config.js
@@ -1,5 +1,7 @@
 const API_URL = "https://script.google.com/macros/s/AKfycbw4-1hWuvY0HKhjgK1XkooqeXiTpxuWlbuX--7xqCxXcfvsxDuAxerzmNFGf-G265Wk0A/exec";
 
+const R2_UPLOAD_URL = "https://tcu-image-upload.redminotee5190.workers.dev";
+
 const CLOUDINARY_URL = "https://api.cloudinary.com/v1_1/db00zaan0/image/upload";
 
 const CLOUDINARY_UPLOAD_PRESET = "maintenance_upload";

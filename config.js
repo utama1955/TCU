c6edc8f61ad8531ea9f7a9e3f12ee3a8ba037067
utama1955 <redminotee5190@gmail.com>
2026-06-27 14:19:04 +0700

    Update API_URL to new endpoint

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const API_URL = "https://script.google.com/macros/s/AKfycbxZUS7M9zR8REg2w7c9i_CCqdmwT3UaTwj6IHk-Rwo3mHwOFYQWn_kkcyRXgPIBFMrDYw/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbwJTlYPgNNrNeqk36moYLzxBmEXIYkFwMACWzkFQwSYP0YRfiqG9t8SG000GzP9Ksj82w/exec";
 
 const R2_UPLOAD_URL = "https://tcu-image-upload.redminotee5190.workers.dev";
 
